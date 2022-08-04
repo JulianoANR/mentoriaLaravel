2e503bf7db426eb22fd4868bba0ee1bd58489a68
Vitor Rodrigues <joa.fggf3.27@gmail.com>
2022-08-04 17:04:37 -0300

    Create Users 1.0

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+        $requestData = $request->all();
+        $requestData['role'] = 'participant';
+        $password = bcrypt($requestData['password']);
+        $requestData['password'] = $password;
+        User::create($requestData);
+    }
+}
